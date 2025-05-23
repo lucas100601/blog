63566cc6ed52db9a3824ca26549120f006682aec
lucas100601 <lucas100601.corp@gmail.com>
2025-05-23 16:58:33 +0800

    chore: correct external repository

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,145 +1,140 @@
-import type * as Preset from '@docusaurus/preset-classic';
-import type { Config } from '@docusaurus/types';
-import { themes as prismThemes } from 'prism-react-renderer';
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Logic Lumina',
-  tagline: '我想寫下的，不只是解法，也是那些「為什麼」',
-  favicon: 'img/favicon.ico',
+    title: "Logic Lumina",
+    tagline: "我想寫下的，不只是解法，也是那些「為什麼」",
+    favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: 'https://lucas100601.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: "https://lucas100601.github.io",
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'lucas100601', // Usually your GitHub org/user name.
-  projectName: 'docusaurus.github.io', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "lucas100601", // Usually your GitHub org/user name.
+    projectName: "lucas100601.github.io", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'zh-TW',
-    locales: ['en','zh-TW'],
-    localeConfigs: {
-      en: {
-        label: 'English',
-        direction: 'ltr',       
-      },
-      'zh-TW': {
-        label: '繁體中文（台灣）',
-        direction: 'ltr',
-      },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "zh-TW",
+        locales: ["en", "zh-TW"],
+        localeConfigs: {
+            en: {
+                label: "English",
+                direction: "ltr",
+            },
+            "zh-TW": {
+                label: "繁體中文（台灣）",
+                direction: "ltr",
+            },
+        },
     },
-  },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    presets: [
+        [
+            "classic",
+            {
+                docs: {
+                    sidebarPath: "./sidebars.ts",
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ["rss", "atom"],
+                        xslt: true,
+                    },
+                    // Useful options to enforce blogging best practices
+                    onInlineTags: "warn",
+                    onInlineAuthors: "warn",
+                    onUntruncatedBlogPosts: "warn",
+                },
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: '',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        // {
-        //   type: 'docSidebar',
-        //   sidebarId: 'tutorialSidebar',
-        //   position: 'left',
-        //   label: 'Tutorial',
-        // },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/lucas100601',
-          label: 'GitHub',
-          position: 'right',
+    themeConfig: {
+        // Replace with your project's social card
+        image: "img/docusaurus-social-card.jpg",
+        navbar: {
+            title: "",
+            logo: {
+                alt: "My Site Logo",
+                src: "img/logo.svg",
+            },
+            items: [
+                // {
+                //   type: 'docSidebar',
+                //   sidebarId: 'tutorialSidebar',
+                //   position: 'left',
+                //   label: 'Tutorial',
+                // },
+                { to: "/blog", label: "Blog", position: "left" },
+            ],
         },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      // links: [
-      //   {
-      //     title: 'Docs',
-      //     items: [
-      //       {
-      //         label: 'Tutorial',
-      //         to: '/docs/intro',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: 'Community',
-      //     items: [
-      //       {
-      //         label: 'Stack Overflow',
-      //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-      //       },
-      //       {
-      //         label: 'Discord',
-      //         href: 'https://discordapp.com/invite/docusaurus',
-      //       },
-      //       {
-      //         label: 'X',
-      //         href: 'https://x.com/docusaurus',
-      //       },
-      //     ],
-      //   },
-        // {
-        //   title: 'More',
-        //   items: [
-        //     // {
-        //     //   label: 'Blog',
-        //     //   to: '/blog',
-        //     // },
-        //     {
-        //       label: 'GitHub',
-        //       href: 'https://github.com/lucas100601',
-        //     },
-        //   ],
-        // },
-      // ],
-      copyright: `Copyright © 2025 Lucas Wu. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+        footer: {
+            style: "dark",
+            // links: [
+            //   {
+            //     title: 'Docs',
+            //     items: [
+            //       {
+            //         label: 'Tutorial',
+            //         to: '/docs/intro',
+            //       },
+            //     ],
+            //   },
+            //   {
+            //     title: 'Community',
+            //     items: [
+            //       {
+            //         label: 'Stack Overflow',
+            //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            //       },
+            //       {
+            //         label: 'Discord',
+            //         href: 'https://discordapp.com/invite/docusaurus',
+            //       },
+            //       {
+            //         label: 'X',
+            //         href: 'https://x.com/docusaurus',
+            //       },
+            //     ],
+            //   },
+            // {
+            //   title: 'More',
+            //   items: [
+            //     // {
+            //     //   label: 'Blog',
+            //     //   to: '/blog',
+            //     // },
+            //     {
+            //       label: 'GitHub',
+            //       href: 'https://github.com/lucas100601',
+            //     },
+            //   ],
+            // },
+            // ],
+            copyright: `Copyright © 2025 Lucas Wu. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
